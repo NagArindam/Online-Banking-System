57368b0ff90f5b406e761a7b01de83ae9d715f94
NagArindam <inspkolkata12@gmail.com>
2022-12-14 21:49:16 +0530

    Day2 added

---
+++ Online Banking System/src/masai/project/Exception/CustomerException.java
@@ -0,0 +1,15 @@
+package masai.project.Exception;
+
+public class CustomerException extends Exception{
+	
+	public CustomerException() {
+		
+	}
+	
+	public CustomerException(String message) {
+		super(message);
+	}
+	
+	
+	
+}
